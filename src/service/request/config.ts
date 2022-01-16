cd46536b09e8a71ff297ea7ddf0032c5303d8383
caijianhaoa <1784581164@qq.com>
2022-01-16 20:21:19 +0800

    feat(ghfgfh): ghgfhf
    ghffggf

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -13,15 +13,18 @@
 // 生成环境: production
 // 测试环境: test
 
-let BASE_URL = ''
-const TIME_OUT = 10000
+// let BASE_URL = ''
+// const TIME_OUT = 10000
 
-if (process.env.NODE_ENV === 'development') {
-  BASE_URL = '/api'
-} else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod'
-} else {
-  BASE_URL = 'http://coderwhy.org/test'
-}
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = '/api'
+// } else if (process.env.NODE_ENV === 'production') {
+//   BASE_URL = 'http://coderwhy.org/prod'
+// } else {
+//   BASE_URL = 'http://coderwhy.org/test'
+// }
 
-export { BASE_URL, TIME_OUT }
+export const BASE_URL = process.env.VUE_APP_BASE_URL
+console.log(process.env.VUE_APP_BASE_URL)
+
+export const TIME_OUT = 10000
